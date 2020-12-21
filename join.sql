0a8c2a8aff15cb0375f07126963a9c92d8b0f34c
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2020-12-21 10:53:30 +0530

    Update join.sql

--- join.sql
+++ join.sql
@@ -4,5 +4,22 @@ INSERT INTO branch VALUES(4, "Buffalo", NULL, NULL);
 SELECT employee.emp_id, employee.first_name, branch.branch_name
 FROM employee
 JOIN branch    
--- LEFT JOIN, RIGHT JOIN
 ON employee.emp_id = branch.mgr_id;
+
+--LEFT JOIN
+SELECT employee.emp_id,employee.first_name,branch.branch_name
+FROM employee
+LEFT JOIN branch
+ON employee.emp_id=branch.mgr_id;
+
+--RIGHT JOIN
+SELECT employee.emp_id,employee.first_name,branch.branch_name
+FROM employee
+RIGHT JOIN branch
+ON employee.emp_id=branch.mgr_id;
+
+--FULL JOIN 
+SELECT employee.emp_id,employee.first_name,branch.branch_name
+FROM employee
+FULL JOIN branch
+ON employee.emp_id=branch.mgr_id;
